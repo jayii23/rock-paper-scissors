2c4ea9aba014b9b4b6a460c474eb433bb1201b4b
jayii23 <jkersh998@gmail.com>
2022-01-17 22:07:15 -0800

    Created function that converts user's choice to proper capitalization and compares to computer's choice to determine winner of a round

--- gameplay.js
+++ gameplay.js
@@ -1,5 +1,3 @@
-console.log("Hello World!")
-
 //INSTRUCTIONS --> https://www.theodinproject.com/paths/foundations/courses/foundations/lessons/rock-paper-scissors#assignment
 
 //Function computerPlay - have computer randomly choose rock, paper, scissors
@@ -10,7 +8,7 @@ function computerPlay() {
     let moveID = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
 
     switch (moveID) {
-        case (1): move = "Rock!"
+        case (1): move = "Rock"
         break;
         
         case (2): move = "Paper";
@@ -24,14 +22,45 @@ function computerPlay() {
     return move;
 }
 
-console.log(computerPlay());
-
-
 //Function playRound(playerSelection, computerSelection) - play 1 round of game
     //return string that declares winner of round
     //get computerSelection using function computerPlay
     //make players selection case-insensitive (rock, rOck, etc..)
     //make sure to return results, not just console.log them
+
+function playRound(playerSelection, computerSelection) {
+
+    //convert players selection to properly capitalized move
+    playerSelection.toLowerCase();
+    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
+
+    if (playerSelection == computerSelection) {
+        return "This round was a draw!";
+    }
+
+    if (playerSelection == "Rock" && computerSelection == "Paper") {
+        return `You lose! ${computerSelection} beats ${playerSelection}`;
+    }
+    else if (playerSelection == "Rock" && computerSelection == "Scissors") {
+        return `You win! ${playerSelection} beats ${computerSelection}`;
+    }
+
+    if (playerSelection == "Paper" && computerSelection == "Rock") {
+        return `You win! ${playerSelection} beats ${computerSelection}`;
+    }
+    else if (playerSelection == "Paper" && computerSelection == "Scissors") {
+        return `You lose! ${computerSelection} beats ${playerSelection}`;
+    }
+    
+    if (playerSelection == "Scissors" && computerSelection == "Rock") {
+        return `You lose! ${computerSelection} beats ${playerSelection}`;
+    }
+    else if (playerSelection == "Scissors" && computerSelection == "Paper") {
+        return `You win! ${playerSelection} beats ${computerSelection}`;
+    }
+
+}
+
 
 //Function game() - play a 5 round game and keep score
     //must use previous function within it
