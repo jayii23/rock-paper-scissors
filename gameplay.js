47c45d2a5b4f13c1d038353517cd423f91a25eeb
jayii23 <jkersh998@gmail.com>
2022-01-17 21:26:59 -0800

    Completed a function that randomly returns rock, paper, or scissors for the computer's turn

--- gameplay.js
+++ gameplay.js
@@ -3,6 +3,29 @@ console.log("Hello World!")
 //INSTRUCTIONS --> https://www.theodinproject.com/paths/foundations/courses/foundations/lessons/rock-paper-scissors#assignment
 
 //Function computerPlay - have computer randomly choose rock, paper, scissors
+function computerPlay() {
+    let move;
+    let min = Math.ceil(3);
+    let max = Math.floor(1);
+    let moveID = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
+
+    switch (moveID) {
+        case (1): move = "Rock!"
+        break;
+        
+        case (2): move = "Paper";
+        break;
+
+        case (3): move = "Scissors";
+        break;
+
+    }
+
+    return move;
+}
+
+console.log(computerPlay());
+
 
 //Function playRound(playerSelection, computerSelection) - play 1 round of game
     //return string that declares winner of round
