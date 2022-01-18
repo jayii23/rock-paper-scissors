af9f6a8da9522761247989fc6846fd4b86bf1496
jayii23 <jkersh998@gmail.com>
2022-01-17 18:07:11 -0800

    Added boilerplate html/css, wrote down basic idea for project in pseudocode in js

---
+++ gameplay.js
@@ -0,0 +1,17 @@
+console.log("Hello World!")
+
+//INSTRUCTIONS --> https://www.theodinproject.com/paths/foundations/courses/foundations/lessons/rock-paper-scissors#assignment
+
+//Function computerPlay - have computer randomly choose rock, paper, scissors
+
+//Function playRound(playerSelection, computerSelection) - play 1 round of game
+    //return string that declares winner of round
+    //get computerSelection using function computerPlay
+    //make players selection case-insensitive (rock, rOck, etc..)
+    //make sure to return results, not just console.log them
+
+//Function game() - play a 5 round game and keep score
+    //must use previous function within it
+    //use console.log to display results of each round and overall winner
+    //use prompt() to get input from user
+    //feel free to create more helper functions
